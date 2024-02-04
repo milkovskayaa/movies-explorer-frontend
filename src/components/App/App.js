07cbef0e58e98987046f7718cd90c83154c2c5f7
Tatyana Maltseva <t.malcevaa14@gmail.com>
2024-02-04 15:46:17 +0300

    feat: сверстать компонент Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import Header from "../Header/Header";
+import Main from '../Main/Main';
 
 function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='root'>
       <Header loggedIn={loggedIn} />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Promo from '../Promo/Promo';
+
+function Main() {
+  return(
+    <main className='content'>
+      <Promo />
+    </main>
+
+  )
+}
+
+export default Main;
