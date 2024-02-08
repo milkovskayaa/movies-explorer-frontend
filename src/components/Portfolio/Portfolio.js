f9b87b7e6cd7b72f4592e9726504bd82de434871
Tatyana Maltseva <t.malcevaa14@gmail.com>
2024-02-08 21:53:31 +0300

    feat: сдлать верстку компонента Portfolio

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Portfolio() {
+  return(
+    <section className='portfolio'>
+      <div className='portfolio__container'>
+        <h2 className='portfolio__title'>Портфолио</h2>
+        <ul className='portfolio__list'>
+          <li className='portfolio__item'>
+            <a className='portfolio__item-link' href='https://github.com/milkovskayaa/how-to-learn'>
+              Статичный сайт
+              <button type='button' className='portfolio__item-button'></button>
+            </a>
+          </li>
+          <li className='portfolio__item'>
+            <a className='portfolio__item-link' href='https://milkovskayaa.github.io/russian-travel/index.html'>
+              Адаптивный сайт
+              <button type='button' className='portfolio__item-button'></button>
+            </a>
+          </li>
+          <li className='portfolio__item'>
+            <a className='portfolio__item-link' href='https://tmalceva.nomoredomainsmonster.ru'>
+              Одностраничное приложение
+              <button type='button' className='portfolio__item-button'></button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio;
